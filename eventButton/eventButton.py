84534cfe260531f1eb3b30e63329fc24820327fb
wohhie <wohhie92@gmail.com>
2016-12-23 23:40:24 +0600

    Update the button event

--- eventButton/eventButton.py
+++ eventButton/eventButton.py
@@ -12,6 +12,29 @@ class Window(Frame):
     def init_window(self):
         self.master.title("GUI")
         self.pack(fill=BOTH, expand=1)
+
+        #creating a Menu
+        menu = Menu(self.master)
+        self.master.config(menu = menu)
+
+        #create a menu
+        file = Menu(menu)
+        #add Edit option
+        file.add_command(label='Settings')
+        #add Exit Option
+        file.add_command(label='Exit', command=self.client_exit)
+        
+
+        #add the submenu to menu
+        menu.add_cascade(label='File', menu = file)
+
+
+        #add another undo menu
+        edit = Menu(menu)
+        edit.add_command(label='Undo')
+
+        menu.add_cascade(label='Edit', menu=edit)
+
         
         #cleare complete Button
         completeButton = Button(self, text = "Complete", command = self.client_exit)
